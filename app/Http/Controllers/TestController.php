89cd0ee63015d4c410fabee8cf19a6a55314ed9c
Piyush Raut <piyushraut0001@gmail.com>
2016-12-17 14:09:48 +0530

    Initial Commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+
+public function index()
+{
+	return "This is index of Test";
+
+
+}
+
+{
+	
